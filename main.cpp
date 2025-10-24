0f422e26f6e2453be75ca3892aba577b15715f14
Aymeric Wibo <obiwac@gmail.com>
2025-10-25 00:33:21 +0200

    openxr: Polling and handling events

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
+#include <cassert>
 #include <jni.h>
 #include <string.h>
-#include <cassert>
 
 #include <EGL/egl.h>
 #include <GLES/gl.h>
@@ -15,18 +15,15 @@
 #include <openxr/openxr.h>
 #include <openxr/openxr_platform.h>
 
-#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "native-activity", __VA_ARGS__))
-#define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN, "native-activity", __VA_ARGS__))
-#define LOGE(...) ((void)__android_log_print(ANDROID_LOG_ERROR, "native-activity", __VA_ARGS__))
+#define LOGI(...) ((void) __android_log_print(ANDROID_LOG_INFO, "native-activity", __VA_ARGS__))
+#define LOGW(...) ((void) __android_log_print(ANDROID_LOG_WARN, "native-activity", __VA_ARGS__))
+#define LOGE(...) ((void) __android_log_print(ANDROID_LOG_ERROR, "native-activity", __VA_ARGS__))
 
-/**
- * Our saved state data.
- */
-struct saved_state {
+typedef struct {
 	float angle;
 	int32_t x;
 	int32_t y;
-};
+} state_t;
 
 /**
  * Shared state for our app.
@@ -40,7 +37,7 @@ struct engine {
 	EGLContext context;
 	int32_t width;
 	int32_t height;
-	struct saved_state state;
+	state_t state;
 };
 
 /**
@@ -54,12 +51,8 @@ static int engine_init_display(struct engine* engine) {
 	 * Below, we select an EGLConfig with at least 8 bits per color
 	 * component compatible with on-screen windows
 	 */
-	const EGLint attrs[] = {
-			EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
-			EGL_BLUE_SIZE, 8,
-			EGL_GREEN_SIZE, 8,
-			EGL_RED_SIZE, 8,
-			EGL_NONE
+	EGLint const attrs[] = {
+		EGL_SURFACE_TYPE, EGL_WINDOW_BIT, EGL_BLUE_SIZE, 8, EGL_GREEN_SIZE, 8, EGL_RED_SIZE, 8, EGL_NONE
 	};
 	EGLint w, h, format;
 	EGLint numConfigs;
@@ -74,7 +67,7 @@ static int engine_init_display(struct engine* engine) {
 	/* Here, the application chooses the configuration it desires.
 	 * find the best match if possible, otherwise use the very first one
 	 */
-	eglChooseConfig(display, attrs, nullptr,0, &numConfigs);
+	eglChooseConfig(display, attrs, nullptr, 0, &numConfigs);
 	auto supportedConfigs = new EGLConfig[numConfigs];
 	assert(supportedConfigs);
 	eglChooseConfig(display, attrs, supportedConfigs, numConfigs, &numConfigs);
@@ -83,12 +76,11 @@ static int engine_init_display(struct engine* engine) {
 	for (; i < numConfigs; i++) {
 		auto& cfg = supportedConfigs[i];
 		EGLint r, g, b, d;
-		if (eglGetConfigAttrib(display, cfg, EGL_RED_SIZE, &r)   &&
-			eglGetConfigAttrib(display, cfg, EGL_GREEN_SIZE, &g) &&
-			eglGetConfigAttrib(display, cfg, EGL_BLUE_SIZE, &b)  &&
-			eglGetConfigAttrib(display, cfg, EGL_DEPTH_SIZE, &d) &&
-			r == 8 && g == 8 && b == 8 && d == 0 ) {
-
+		if (eglGetConfigAttrib(display, cfg, EGL_RED_SIZE, &r) &&
+		    eglGetConfigAttrib(display, cfg, EGL_GREEN_SIZE, &g) &&
+		    eglGetConfigAttrib(display, cfg, EGL_BLUE_SIZE, &b) &&
+		    eglGetConfigAttrib(display, cfg, EGL_DEPTH_SIZE, &d) &&
+		    r == 8 && g == 8 && b == 8 && d == 0) {
 			config = supportedConfigs[i];
 			break;
 		}
@@ -145,8 +137,7 @@ static void engine_draw_frame(struct engine* engine) {
 	}
 
 	// Just fill the screen with a color.
-	glClearColor(((float)engine->state.x)/engine->width, engine->state.angle,
-				 ((float)engine->state.y)/engine->height, 1);
+	glClearColor(((float) engine->state.x) / engine->width, engine->state.angle, ((float) engine->state.y) / engine->height, 1);
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	eglSwapBuffers(engine->display, engine->surface);
@@ -176,7 +167,7 @@ static void engine_term_display(struct engine* engine) {
  * Process the next input event.
  */
 static int32_t engine_handle_input(struct android_app* app, AInputEvent* event) {
-	struct engine* engine = (struct engine*)app->userData;
+	struct engine* engine = (struct engine*) app->userData;
 	if (AInputEvent_getType(event) == AINPUT_EVENT_TYPE_MOTION) {
 		engine->animating = 1;
 		engine->state.x = AMotionEvent_getX(event, 0);
@@ -190,32 +181,32 @@ static int32_t engine_handle_input(struct android_app* app, AInputEvent* event) 
  * Process the next main command.
  */
 static void engine_handle_cmd(struct android_app* app, int32_t cmd) {
-	struct engine* engine = (struct engine*)app->userData;
+	struct engine* engine = (struct engine*) app->userData;
 	switch (cmd) {
-		case APP_CMD_SAVE_STATE:
-			// The system has asked us to save our current state.  Do so.
-			engine->app->savedState = malloc(sizeof(struct saved_state));
-			*((struct saved_state*)engine->app->savedState) = engine->state;
-			engine->app->savedStateSize = sizeof(struct saved_state);
-			break;
-		case APP_CMD_INIT_WINDOW:
-			// The window is being shown, get it ready.
-			if (engine->app->window != NULL) {
-				engine_init_display(engine);
-				engine_draw_frame(engine);
-			}
-			break;
-		case APP_CMD_TERM_WINDOW:
-			// The window is being hidden or closed, clean it up.
-			engine_term_display(engine);
-			break;
-		case APP_CMD_GAINED_FOCUS:
-			break;
-		case APP_CMD_LOST_FOCUS:
-			// Also stop animating.
-			engine->animating = 0;
+	case APP_CMD_SAVE_STATE:
+		// The system has asked us to save our current state.  Do so.
+		engine->app->savedState = malloc(sizeof engine->state);
+		*((state_t*) engine->app->savedState) = engine->state;
+		engine->app->savedStateSize = sizeof engine->state;
+		break;
+	case APP_CMD_INIT_WINDOW:
+		// The window is being shown, get it ready.
+		if (engine->app->window != NULL) {
+			engine_init_display(engine);
 			engine_draw_frame(engine);
-			break;
+		}
+		break;
+	case APP_CMD_TERM_WINDOW:
+		// The window is being hidden or closed, clean it up.
+		engine_term_display(engine);
+		break;
+	case APP_CMD_GAINED_FOCUS:
+		break;
+	case APP_CMD_LOST_FOCUS:
+		// Also stop animating.
+		engine->animating = 0;
+		engine_draw_frame(engine);
+		break;
 	}
 }
 
@@ -275,7 +266,7 @@ void android_main(struct android_app* app) {
 		return;
 	}
 
-	for (auto &api_layer : api_layers) {
+	for (auto& api_layer : api_layers) {
 		LOGI("OpenXR runtime supports '%s' API layer v%lu.", api_layer.layerName, api_layer.specVersion);
 	}
 
@@ -297,19 +288,19 @@ void android_main(struct android_app* app) {
 		return;
 	}
 
-	auto required_exts = std::vector{
+	auto required_exts = std::vector {
 		XR_KHR_OPENGL_ES_ENABLE_EXTENSION_NAME,
 		XR_EXT_DEBUG_UTILS_EXTENSION_NAME,
 	};
 
-	for (auto &ext : exts) {
+	for (auto& ext : exts) {
 		LOGI("OpenXR runtime supports '%s' extension v%u.", ext.extensionName, ext.extensionVersion);
 	}
 
-	for (auto &required : required_exts) {
+	for (auto& required : required_exts) {
 		bool found = false;
 
-		for (auto &ext : exts) {
+		for (auto& ext : exts) {
 			if (strcmp(required, ext.extensionName) == 0) {
 				found = true;
 			}
@@ -354,7 +345,8 @@ void android_main(struct android_app* app) {
 		return;
 	}
 
-	LOGI("OpenXR runtime: %s %d.%d.%d",
+	LOGI(
+		"OpenXR runtime: %s %d.%d.%d",
 		instance_props.runtimeName,
 		XR_VERSION_MAJOR(instance_props.runtimeVersion),
 		XR_VERSION_MINOR(instance_props.runtimeVersion),
@@ -404,7 +396,8 @@ void android_main(struct android_app* app) {
 		return;
 	}
 
-	LOGI("OpenXR OpenGL ES graphics API requirements: min %u.%u.%u, max %u.%u.%u",
+	LOGI(
+		"OpenXR OpenGL ES graphics API requirements: min %u.%u.%u, max %u.%u.%u",
 		XR_VERSION_MAJOR(graphics_requirements.minApiVersionSupported),
 		XR_VERSION_MINOR(graphics_requirements.minApiVersionSupported),
 		XR_VERSION_PATCH(graphics_requirements.minApiVersionSupported),
@@ -479,7 +472,8 @@ void android_main(struct android_app* app) {
 	// Create an EGL context.
 
 	EGLint const context_attrs[] = {
-		EGL_CONTEXT_CLIENT_VERSION, XR_VERSION_MAJOR(graphics_requirements.maxApiVersionSupported),
+		EGL_CONTEXT_CLIENT_VERSION,
+		XR_VERSION_MAJOR(graphics_requirements.maxApiVersionSupported),
 		// EGL_CONTEXT_PRIORITY_LEVEL_IMG, EGL_CONTEXT_PRIORITY_LOW_IMG,
 		EGL_NONE,
 	};
@@ -493,12 +487,7 @@ void android_main(struct android_app* app) {
 
 	// Create an EGL surface.
 
-	EGLint const surf_attrs[] = {
-		EGL_WIDTH, 16,
-		EGL_HEIGHT, 16,
-		EGL_NONE,
-	};
-
+	EGLint const surf_attrs[] = {EGL_WIDTH, 16, EGL_HEIGHT, 16, EGL_NONE};
 	EGLSurface const tiny_surf = eglCreatePbufferSurface(display, selected_config, surf_attrs);
 
 	if (tiny_surf == EGL_NO_SURFACE) {
@@ -528,49 +517,130 @@ void android_main(struct android_app* app) {
 		return;
 	}
 
-	// TODO Clean up the following code.
+	XrSessionState session_state = XR_SESSION_STATE_UNKNOWN;
+
+	// Starting from a saved state; restore it.
 
 	if (app->savedState != NULL) {
-		// We are starting with a previous saved state; restore from it.
-		engine.state = *(struct saved_state*)app->savedState;
+		engine.state = *(state_t*) app->savedState;
 	}
 
-	// loop waiting for stuff to do.
+	// Main loop.
 
-	while (1) {
-		// Read all pending events.
-		int ident;
+	bool running = true;
+	bool session_running = false;
+
+	for (; running;) {
+		// Read Android events.
+
 		int events;
-		struct android_poll_source* source;
+		struct android_poll_source* source = nullptr;
 
-		// If not animating, we will block forever waiting for events.
-		// If animating, we loop until all events are read, then continue
-		// to draw the next frame of animation.
-		while ((ident=ALooper_pollOnce(engine.animating ? 0 : -1, NULL, &events,
-									  (void**)&source)) >= 0) {
-
-			// Process this event.
-			if (source != NULL) {
-				source->process(app, source);
+		while (ALooper_pollOnce(0, nullptr, &events, (void**) &source) >= 0) {
+			if (source == nullptr) {
+				break;
 			}
 
-			// Check if we are exiting.
-			if (app->destroyRequested != 0) {
-				engine_term_display(&engine);
-				return;
-			}
+			source->process(app, source);
 		}
 
-		if (engine.animating) {
-			// Done with events; draw next animation frame.
-			engine.state.angle += .01f;
-			if (engine.state.angle > 1) {
-				engine.state.angle = 0;
-			}
+		// Read OpenXR events.
 
-			// Drawing is throttled to the screen update rate, so there
-			// is no need to do timing here.
-			engine_draw_frame(&engine);
+		for (
+			XrEventDataBuffer event = {XR_TYPE_EVENT_DATA_BUFFER};
+			xrPollEvent(inst, &event) == XR_SUCCESS;
+			event = {XR_TYPE_EVENT_DATA_BUFFER}
+		) {
+			switch (event.type) {
+			case XR_TYPE_EVENT_DATA_EVENTS_LOST: {
+				XrEventDataEventsLost* const events_lost = reinterpret_cast<XrEventDataEventsLost*>(&event);
+				LOGW("%d OpenXR events lost.", events_lost->lostEventCount);
+				break;
+			}
+			case XR_TYPE_EVENT_DATA_INSTANCE_LOSS_PENDING: {
+				XrEventDataInstanceLossPending* const inst_loss_pending = reinterpret_cast<XrEventDataInstanceLossPending*>(&event);
+				LOGW("OpenXR instance loss pending at %ld", inst_loss_pending->lossTime);
+
+				running = false;
+				session_running = false;
+
+				break;
+			}
+			case XR_TYPE_EVENT_DATA_INTERACTION_PROFILE_CHANGED: {
+				XrEventDataInteractionProfileChanged* const interaction_profile_change = reinterpret_cast<XrEventDataInteractionProfileChanged*>(&event);
+				LOGI("Interaction profile changed for our OpenXR session.");
+
+				if (interaction_profile_change->session != session) {
+					LOGW("Interaction profile changed for unknown OpenXR session %p.", interaction_profile_change->session);
+				}
+
+				break;
+			}
+			case XR_TYPE_EVENT_DATA_REFERENCE_SPACE_CHANGE_PENDING: {
+				XrEventDataReferenceSpaceChangePending* const ref_space_change_pending = reinterpret_cast<XrEventDataReferenceSpaceChangePending*>(&event);
+				LOGI("Reference space change pending for our OpenXR session.");
+
+				if (ref_space_change_pending->session != session) {
+					LOGW("Reference space change pending for unknown OpenXR session %p.", ref_space_change_pending->session);
+				}
+
+				break;
+			}
+			case XR_TYPE_EVENT_DATA_SESSION_STATE_CHANGED: {
+				XrEventDataSessionStateChanged* const session_state_change = reinterpret_cast<XrEventDataSessionStateChanged*>(&event);
+
+				if (session_state_change->session != session) {
+					LOGW("State changed for unknown OpenXR session %p.", session_state_change->session);
+					break;
+				}
+
+				switch (session_state_change->state) {
+				case XR_SESSION_STATE_READY: {
+					XrSessionBeginInfo const session_begin_info = {
+						.type = XR_TYPE_SESSION_BEGIN_INFO,
+						.primaryViewConfigurationType = XR_VIEW_CONFIGURATION_TYPE_PRIMARY_STEREO,
+					};
+
+					XrResult const res = xrBeginSession(session, &session_begin_info);
+
+					if (res != XR_SUCCESS) {
+						LOGW("xrBeginSession failed: %d.", res);
+					}
+
+					else {
+						session_running = true;
+					}
+
+					break;
+				}
+				case XR_SESSION_STATE_STOPPING: {
+					XrResult const res = xrEndSession(session);
+
+					if (res != XR_SUCCESS) {
+						LOGW("xrEndSession failed: %d.", res);
+					}
+
+					session_running = false;
+					break;
+				}
+				case XR_SESSION_STATE_EXITING:
+					session_running = false;
+					running = false;
+					break;
+				case XR_SESSION_STATE_LOSS_PENDING:
+					session_running = false;
+					running = false;
+					break;
+
+				default:
+					break;
+				}
+
+				session_state = session_state_change->state;
+				break;
+			}
+			default:;
+			}
 		}
 	}
 
