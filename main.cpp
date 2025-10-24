b052a6c9a936633f5e14c3ed062e2ac343b3e10a
Aymeric Wibo <obiwac@gmail.com>
2025-10-25 01:08:37 +0200

    android: Remove a lot of the previous and now-irrelevant EGL init code

--- main.cpp
+++ main.cpp
@@ -41,93 +41,6 @@ struct engine {
 };
 
 /**
- * Initialize an EGL context for the current display.
- */
-static int engine_init_display(struct engine* engine) {
-	// initialize OpenGL ES and EGL
-
-	/*
-	 * Here specify the attributes of the desired configuration.
-	 * Below, we select an EGLConfig with at least 8 bits per color
-	 * component compatible with on-screen windows
-	 */
-	EGLint const attrs[] = {
-		EGL_SURFACE_TYPE, EGL_WINDOW_BIT, EGL_BLUE_SIZE, 8, EGL_GREEN_SIZE, 8, EGL_RED_SIZE, 8, EGL_NONE
-	};
-	EGLint w, h, format;
-	EGLint numConfigs;
-	EGLConfig config;
-	EGLSurface surface;
-	EGLContext context;
-
-	EGLDisplay display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
-
-	eglInitialize(display, 0, 0);
-
-	/* Here, the application chooses the configuration it desires.
-	 * find the best match if possible, otherwise use the very first one
-	 */
-	eglChooseConfig(display, attrs, nullptr, 0, &numConfigs);
-	auto supportedConfigs = new EGLConfig[numConfigs];
-	assert(supportedConfigs);
-	eglChooseConfig(display, attrs, supportedConfigs, numConfigs, &numConfigs);
-	assert(numConfigs);
-	auto i = 0;
-	for (; i < numConfigs; i++) {
-		auto& cfg = supportedConfigs[i];
-		EGLint r, g, b, d;
-		if (eglGetConfigAttrib(display, cfg, EGL_RED_SIZE, &r) &&
-		    eglGetConfigAttrib(display, cfg, EGL_GREEN_SIZE, &g) &&
-		    eglGetConfigAttrib(display, cfg, EGL_BLUE_SIZE, &b) &&
-		    eglGetConfigAttrib(display, cfg, EGL_DEPTH_SIZE, &d) &&
-		    r == 8 && g == 8 && b == 8 && d == 0) {
-			config = supportedConfigs[i];
-			break;
-		}
-	}
-	if (i == numConfigs) {
-		config = supportedConfigs[0];
-	}
-
-	/* EGL_NATIVE_VISUAL_ID is an attribute of the EGLConfig that is
-	 * guaranteed to be accepted by ANativeWindow_setBuffersGeometry().
-	 * As soon as we picked a EGLConfig, we can safely reconfigure the
-	 * ANativeWindow buffers to match, using EGL_NATIVE_VISUAL_ID. */
-	eglGetConfigAttrib(display, config, EGL_NATIVE_VISUAL_ID, &format);
-	surface = eglCreateWindowSurface(display, config, engine->app->window, NULL);
-	context = eglCreateContext(display, config, NULL, NULL);
-
-	if (eglMakeCurrent(display, surface, surface, context) == EGL_FALSE) {
-		LOGW("Unable to eglMakeCurrent");
-		return -1;
-	}
-
-	eglQuerySurface(display, surface, EGL_WIDTH, &w);
-	eglQuerySurface(display, surface, EGL_HEIGHT, &h);
-
-	engine->display = display;
-	engine->context = context;
-	engine->surface = surface;
-	engine->width = w;
-	engine->height = h;
-	engine->state.angle = 0;
-
-	// Check openGL on the system
-	auto opengl_info = {GL_VENDOR, GL_RENDERER, GL_VERSION, GL_EXTENSIONS};
-	for (auto name : opengl_info) {
-		auto info = glGetString(name);
-		LOGI("OpenGL Info: %s", info);
-	}
-	// Initialize GL state.
-	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_FASTEST);
-	glEnable(GL_CULL_FACE);
-	glShadeModel(GL_SMOOTH);
-	glDisable(GL_DEPTH_TEST);
-
-	return 0;
-}
-
-/**
  * Just the current frame in the display.
  */
 static void engine_draw_frame(struct engine* engine) {
@@ -188,13 +101,6 @@ static void engine_handle_cmd(struct android_app* app, int32_t cmd) {
 		engine->app->savedState = malloc(sizeof engine->state);
 		*((state_t*) engine->app->savedState) = engine->state;
 		engine->app->savedStateSize = sizeof engine->state;
-		break;
-	case APP_CMD_INIT_WINDOW:
-		// The window is being shown, get it ready.
-		if (engine->app->window != NULL) {
-			engine_init_display(engine);
-			engine_draw_frame(engine);
-		}
 		break;
 	case APP_CMD_TERM_WINDOW:
 		// The window is being hidden or closed, clean it up.
